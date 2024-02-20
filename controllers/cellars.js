b6cf57bc4f2d295348476bf60065c6671ed64d2b
Enric <enricldn@gamil.com>
2024-02-20 10:56:22 +0100

    debug cellar controller

--- controllers/cellars.js
+++ controllers/cellars.js
@@ -15,18 +15,17 @@ module.exports = {
 
   // POST
   postCellar: async (req, res) => {
-    // Asegúrate de que la desestructuración aquí coincide con los campos enviados en la solicitud
-    const { cellarName, description } = req.body; // Corregido para usar cellar_name en lugar de cellar
-    console.log(req.body);
-
-    if (!cellarName) { // Asegurándose de que cellar_name está presente
-      return res.status(400).json({ error: "Cellar name is required" });
-    }
-
     try {
+      const { cellarName, description } = req.body;
+      console.log(req.body);
+
+      if (!cellarName) {
+        return res.status(400).json({ error: "Cellar name is required" });
+      }
+
       const createdCellar = await Cellar.create({
         cellarName,
-        description
+        description,
       });
       console.log('created cellar', createdCellar);
       res.status(201).json({ message: 'Cellar created successfully', cellar: createdCellar });
@@ -41,45 +40,43 @@ module.exports = {
 
   // DELETE
   deleteCellar: async (req, res) => {
-    const { id } = req.params; 
-
     try {
-      const cellar = await Cellar.findByPk(id); 
+      const { id } = req.params;
+      const cellar = await Cellar.findByPk(id);
       if (!cellar) {
-        return res.status(404).json({ error: "Cellar not found" }); 
+        return res.status(404).json({ error: "Cellar not found" });
       }
 
-      await cellar.destroy(); 
+      await cellar.destroy();
       console.log(`Deleted cellar with ID: ${id}`);
-      res.json({ message: `Cellar with ID: ${id} deleted successfully` }); 
+      res.json({ message: `Cellar with ID: ${id} deleted successfully` });
     } catch (error) {
       console.error("Error deleting cellar:", error);
-      res.status(500).json({ error: "Internal Server Error" }); 
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
   // PUT
   updateCellar: async (req, res) => {
-    const { id } = req.params; 
-    const { cellarName, description } = req.body; 
-
     try {
-      const cellar = await Cellar.findByPk(id); 
-      if (!cellarName) {
-        return res.status(404).json({ error: "Cellar not found" }); 
+      const { id } = req.params;
+      const { cellarName, description } = req.body;
+
+      const cellar = await Cellar.findByPk(id);
+      if (!cellar) {
+        return res.status(404).json({ error: "Cellar not found" });
       }
 
       await cellar.update({
         cellarName,
-        description
+        description,
       });
 
       console.log(`Updated cellar with ID: ${id}`);
-      res.json({ message: `Cellar with ID: ${id} updated successfully`, cellar: cellar }); 
+      res.json({ message: `Cellar with ID: ${id} updated successfully`, cellar });
     } catch (error) {
       console.error("Error updating cellar:", error);
-      res.status(500).json({ error: "Internal Server Error" }); 
+      res.status(500).json({ error: "Internal Server Error" });
     }
-  }
+  },
 };
-
