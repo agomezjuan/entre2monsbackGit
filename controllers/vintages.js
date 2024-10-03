c0dc6c855808f8f3e8cb715d2caf0cc1013f1e5f
Enric <enricldn@gamil.com>
2024-10-03 13:40:12 +0200

    feat: crated: *Document for controllers *PUT method for vintages

---
+++ controllers/vintages.js
@@ -0,0 +1,24 @@
+const { where } = require("sequelize");
+const { Label, Grape } = require("../models");
+
+module.exports = {
+  // ** GET
+  getAllLabels: async (req, res) => {
+    try {
+      const labels = await Label.findAll({
+        include: [
+          {
+            model: Grape,
+            as: "grapes",
+          },
+        ],
+      });
+
+      console.log("All labels:", JSON.stringify(labels, null, 2));
+      res.json(labels);
+    } catch (error) {
+      console.error("Error retrieving labels:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+};
