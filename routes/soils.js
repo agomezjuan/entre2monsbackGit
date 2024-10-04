04bf7078df31b246d2fed9e514360718406b811e
Enric <enricldn@gamil.com>
2024-10-04 13:11:38 +0200

    refactor: Update routes/soils.js to use consistent quotation marks and formatting

--- routes/soils.js
+++ routes/soils.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const {
-  getAllSoils, 
-  createSoil, 
-  getSoilById, 
-  updateSoil, 
-  deleteSoil, 
-  deleteSoilByName
-} = require('../controllers/soils');
+  getAllSoils,
+  createSoil,
+  getSoilById,
+  updateSoil,
+  deleteSoil,
+  deleteSoilByName,
+} = require("../controllers/soils");
 
 const router = express.Router();
 
-router.get('/', getAllSoils);
-router.post('/', createSoil);
-router.get('/:id', getSoilById);
-router.put('/:id', updateSoil);
-router.delete('/:id', deleteSoil);
-router.delete('/:name', deleteSoilByName);
+router.get("/", getAllSoils);
+router.post("/", createSoil);
+router.get("/:id", getSoilById);
+router.put("/:id", updateSoil);
+router.delete("/:id", deleteSoil);
+router.delete("/:name", deleteSoilByName);
 
 module.exports = router;
