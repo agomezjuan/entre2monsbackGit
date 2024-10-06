3fd535f250a02dce783c19840e1c3535f1171615
Enric <enricldn@gamil.com>
2024-10-06 12:41:36 +0200

    refactor: Rename labelsCategoriesId to labelCategoriesId in label migration
    Created seed for labels
    
    created seed for labelsCategories

--- database/migrations/20240223192219-label.js
+++ database/migrations/20240223192219-label.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.TEXT,
         allowNull: true,
       },
-      labelsCategoriesId: {
+      labelCategoriesId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
