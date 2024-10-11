9ee2dde2de137417a10ae1a9f54498a0035abda6
Enric <enricldn@gamil.com>
2024-10-11 19:53:04 +0200

    Refactor icons controller to improve code readability and error handling

--- controllers/icons.js
+++ controllers/icons.js
@@ -1,8 +1,7 @@
-const { Icon } = require('../database/models');
+const { Icon } = require("../database/models");
 
 module.exports = {
-  
-  // GET
+  // GET all Icons
   getAllIcons: async (req, res) => {
     try {
       const icons = await Icon.findAll();
@@ -14,68 +13,73 @@ module.exports = {
     }
   },
 
-  // POST
+  // POST a new Icon
   postIcon: async (req, res) => {
-    const { url, description } = req.body; 
+    const { url, description } = req.body;
     console.log(req.body);
 
-    if (!url) { 
-      return res.status(400).json({ error: "Icon is required" });
+    if (!url) {
+      return res.status(400).json({ error: "Icon URL is required" });
     }
 
     try {
-      const createdIcon = await Icon.create({ 
+      const createdIcon = await Icon.create({
         url,
-        description
+        description,
       });
-      console.log('created icon', createdIcon);
-      res.status(201).json({ message: 'Icon created successfully', icon: createdIcon });
+      console.log("Created icon:", createdIcon);
+      res
+        .status(201)
+        .json({ message: "Icon created successfully", icon: createdIcon });
     } catch (error) {
       console.error("Error creating icon:", error);
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        return res.status(400).json({ error: "Icon must be unique" }); 
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json({ error: "Icon must be unique" });
       }
       res.status(500).json({ error: "Internal Server Error" });
     }
   },
 
-  //DELETE
+  // DELETE an Icon by ID
   deleteIcon: async (req, res) => {
-    const { id } = req.params; 
+    const { id } = req.params;
     try {
-      const icon = await Icon.findByPk(id); 
+      const icon = await Icon.findByPk(id);
       if (!icon) {
-        return res.status(404).json({ error: "Icon not found" }); 
+        return res.status(404).json({ error: "Icon not found" });
       }
-      await icon.destroy(); 
-      console.log(`Deleted icon with ID: ${id}`);
-      res.json({ message: `Icon with ID: ${id} deleted successfully` }); 
+
+      await icon.destroy();
+      res.json({ message: `Icon with ID: ${id} deleted successfully` });
     } catch (error) {
       console.error("Error deleting icon:", error);
-      res.status(500).json({ error: "Internal Server Error" }); 
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
-  // PUT
+
+  // PUT update an Icon by ID
   updateIcon: async (req, res) => {
-    const { id } = req.params; 
-    const { icon: newIcon, description: newDescription } = req.body; // Asegurarse de que las propiedades coincidan con el modelo
+    const { id } = req.params;
+    const { url, description } = req.body;
 
     try {
       const iconToUpdate = await Icon.findByPk(id);
       if (!iconToUpdate) {
-        return res.status(404).json({ error: "Icon not found" }); 
+        return res.status(404).json({ error: "Icon not found" });
       }
 
       await iconToUpdate.update({
-        icon: newIcon,
-        description: newDescription, 
+        url,
+        description,
       });
 
-      console.log(`Updated icon with ID: ${id}`); 
-      res.json({ message: `Icon with ID: ${id} updated successfully`, icon: iconToUpdate }); 
+      res.json({
+        message: `Icon with ID: ${id} updated successfully`,
+        icon: iconToUpdate,
+      });
     } catch (error) {
       console.error("Error updating icon:", error);
-      res.status(500).json({ error: "Internal Server Error" }); 
+      res.status(500).json({ error: "Internal Server Error" });
     }
-  }
+  },
 };
